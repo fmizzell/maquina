befcad376e2e486ac59d4b2513338d07d9a587b6
Thierry Dallacroce <thierry.dallacroce@semanticbits.com>
2020-03-12 14:42:09 -0700

    Add ability to start or stop recording execution data

--- src/StateMachine/Execution.php
+++ src/StateMachine/Execution.php
@@ -7,8 +7,13 @@ trait Execution
 {
     public $execution;
 
+    private $recordsData = TRUE;
+
     private function recordStateExecution($next_states)
     {
+        if (!$this->recordsData) {
+            return;
+        }
         if (json_encode($this->currentStates) != json_encode($next_states)) {
             array_push($this->execution, $next_states);
             array_push($this->execution, "");
@@ -17,8 +22,20 @@ trait Execution
 
     private function recordInputExecution($input)
     {
+        if (!$this->recordsData) {
+            return;
+        }
         $inputs = array_pop($this->execution);
         $inputs .= $input;
         array_push($this->execution, $inputs);
     }
+
+    public function startRecording() {
+        $this->recordsData = TRUE;
+    }
+
+    public function stopRecording() {
+        $this->recordsData = FALSE;
+    }
+
 }
